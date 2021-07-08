924f11d8283fdd29b576eacb3bf957ffb22a7354
Johnn <j.christian.vasquez@gmail.com>
2021-07-08 17:14:48 -0500

    Flask Blueprints

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,18 +1,25 @@
-from flask import render_template
-from app import app
+from flask import Blueprint, render_template, redirect
 
 
-@app.route('/')
+bp = Blueprint('', __name__)
+
+
+@bp.route('/')
 def index():
     return render_template('page.html', title='WASSUUUUUP')
     # return '<h1>WASSUPPP</h1>'
 
 
-@app.route('/help')
+@bp.route('/help')
 def help():
     return render_template('page.html', title='HEARD YOU NEEDED SOME HELP')
 
 
-@app.route('/additionalhelp')
+@bp.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@bp.route('/additionalhelp')
 def more_help():
     return render_template('page.html', title='DAMN HOW MUCH HELP YOU NEED')

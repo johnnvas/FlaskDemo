4a1fbdd90fad93943262518168292e249876791a
Johnn <j.christian.vasquez@gmail.com>
2021-07-09 12:12:50 -0500

    Finished Demo

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,18 +1,43 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, session
 from app.forms.login import LoginForm
 
 bp = Blueprint('', __name__)
 
+def track_views():
+    if 'views' in session:
+        session['views'] = session.get('views') + 1
+    else:
+        session['views'] = 1
+
 
 @bp.route('/')
 def index():
+    track_views()
     return render_template('page.html', title='WASSUUUUUP')
     # return '<h1>WASSUPPP</h1>'
 
 
+@bp.route('/views')
+def views():
+    return f'Total views: {session.get("views")}'
+
+
+@bp.route('/views/reset')
+def reset_views():
+    views = session.pop('views', None)
+    return f'Reset views (previous {views})'
+
+
 @bp.route('/help')
 def help():
+    track_views()
     return render_template('page.html', title='HEARD YOU NEEDED SOME HELP')
+
+
+@bp.route('/additionalhelp')
+def more_help():
+    track_views()
+    return render_template('page.html', title='DAMN HOW MUCH HELP YOU NEED')
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -21,8 +46,3 @@ def login():
     if form.validate_on_submit():
         return redirect('/')
     return render_template('login.html', form=form)
-
-
-@bp.route('/additionalhelp')
-def more_help():
-    return render_template('page.html', title='DAMN HOW MUCH HELP YOU NEED')
